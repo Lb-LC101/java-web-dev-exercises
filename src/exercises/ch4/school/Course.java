6300f293b856220997f30308d0fe5ec70f729b1d
Lb-LC101 <lisabartfeld@gmail.com>
2021-01-24 10:36:13 -0600

    Chapter 4 exercises

---
+++ src/exercises/ch4/school/Course.java
@@ -0,0 +1,35 @@
+package exercises.ch4.school;
+
+import java.util.ArrayList;
+
+public class Course {
+    //Name of Course
+    private String name;
+    // List of Students
+    private ArrayList<Student> students;
+    // Worth number of credits
+    private int worthNumberOfCredits;
+
+    // Constructors
+    public Course(String name, int worthNumberOfCredits) {
+        this.name = name;
+        this.worthNumberOfCredits = worthNumberOfCredits;
+       // this.worthNumberOfCredits = numberOfCredits;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " is worth " + this.worthNumberOfCredits + " number of credits.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj instanceof Course) == false) return false;
+        Course myCourseObj = (Course) obj;
+        if (myCourseObj.name.equals(this.name)) {
+            return true;
+        }   else {
+            return false;
+        }
+    }
+}
